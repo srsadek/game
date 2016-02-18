ced3f4507788d8b6fdbdcf7fecb2448306abb7af
Raj Nayak Ammunje <arnayak4u@gmail.com>
2016-02-18 04:26:59 -0500

    Signal R Hub files included

---
+++ 29GameWebApp/Hubs/GameHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace _29GameWebApp.Hubs
+{
+    public class GameHub : Hub
+    {
+        public void Hello()
+        {
+
+        }
+
+        
+    }
+}
